f2b33c66c150e41d782d9018b990e7bedb4da7fc
aioperatorio-eng <ai.operator.io@gmail.com>
2026-01-06 21:28:36 +0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,16 @@
+// Default database config
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', 'db/data.db'),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
